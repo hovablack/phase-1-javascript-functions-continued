e24e667b150e9b951233a1d75963ec0de13348b2
Hova Black <nevermind92@hotmail.com>
2021-11-02 22:01:35 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(params = "roller-skate") {
+  return `This Saturday, I want to ${params}!`
+};
+
+function mondayWork(params = "go to the office") {
+  return `This Monday, I will ${params}.`
+};
+
+function wrapAdjective(params = "*") {
+  return function (innerparams = "special") {
+    return `You are ${params}${innerparams}${params}!`
+  }
+};
